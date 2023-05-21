aa8ed289a6a86ba8f7d7cd66263d14f10ea8948e
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-05-21 22:14:48 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,30 +1,29 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// const input = document.querySelector('#controls input');
-// const createBtn = document.querySelector('[data-create]');
-// const destroyBtn = document.querySelector('[data-destroy]');
-// const boxes = document.querySelector('#boxes');
 
-// createBtn.addEventListener('click', () => createBoxes(input.value));
-// destroyBtn.addEventListener('click', destroyBoxes);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const control = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
 
-// function createBoxes(amount) {
-//   for (let i = 0; i < amount; i++) {
-//     const div = document.createElement('div');
-//     const size = 30 + i * 10;
-//     const color = getRandomHexColor();
+
+createBtn.addEventListener('click', () => createBox(control.value));
+destroyBtn.addEventListener('click', destroyBox);
+function createBox(amount){
+  for (let i = 0; i < amount; i+= 1) {
+    const div = document.createElement('div')
+    const size = 30 + i * 10;
+    const color = getRandomHexColor()
     
-//     div.style.width = size + 'px';
-//     div.style.height = size + 'px';
-//     div.style.backgroundColor = color;
-    
-//     boxes.appendChild(div);
-//   }
-// }
-
-// function destroyBoxes() {
-//   boxes.innerHTML = '';
-// }
+    div.style.height = size + 'px';
+    div.style.width = size + 'px';
+    div.style.backgroundColor= color
+    boxes.append(div)
+  }
+}
+function destroyBox() {
+  boxes.innerHTML= '';
+}
